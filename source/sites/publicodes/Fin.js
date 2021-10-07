9d31b71237f7e0d10bb1910102b93b28088c888b
Mael <mael.thomas@data.gouv.fr>
2021-10-07 16:36:48 +0200

    Pas de lien vers la doc sur le graph de simulation

--- source/sites/publicodes/Fin.js
+++ source/sites/publicodes/Fin.js
@@ -250,6 +250,7 @@ const AnimatedDiv = ({ score, value, details, headlessMode }) => {
 					<div css="padding: 1rem">
 						<Chart
 							details={details}
+							links
 							color={textColor}
 							noAnimation
 							noText
